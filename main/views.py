db1ff5abb3776ca409e9c3de451d08ddccd934c1
Александр <aleksandr@MacBook-Air-Aleksandr.local>
2025-10-11 19:18:59 +0300

    отображение главной страницы, пока не в iframe (надо разбираться с integration_utils

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Главная'
+    }
+    return render(request, 'main/index.html', context)
