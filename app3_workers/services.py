9420362e7b5c7939411c8a988bb594984f079791
Александр <romanovsasa26@gmail.com>
2025-10-19 00:34:08 +0300

    окончательная и абсолютно верная функция иерархического вывода начальников

--- app3_workers/services.py
+++ app3_workers/services.py
@@ -18,39 +18,6 @@ def api_workers_info(but):
     result['department_list'] = but.call_list_method('department.get')
     return result
 
-def get_department_names(department_hash, user):
-    """ Возвращает список отделов для конкретного user """
-
-    return [department_hash.get(dep_id).get('NAME') for dep_id in user.get('UF_DEPARTMENT')]
-
-
-def chief_forward_list(department_hash, users_hash, user):
-    """ Рекурсивная функция для иерархического поиска всех начальников - возвращает список начальников """
-
-    user_id = safe_int(user['ID'])
-    chiefs = []
-    seen_heads = set()
-
-    user_departments = user.get('UF_DEPARTMENT')
-    if not user_departments:
-        return []
-
-    dept_depths = [(dep_id, get_department_depth(dep_id, department_hash)) for dep_id in user_departments]
-    dept_depths.sort(key=lambda x: x[1], reverse=True)  # отдел с максимальной глубиной первым
-    main_dep_id = dept_depths[0][0]
-
-    # отдельно для главного отдела
-    add_chiefs_list(main_dep_id, department_hash, user_id, users_hash, seen_heads, chiefs)
-
-    # оставшиеся отделы
-    for dep_id in user_departments:
-        if dep_id == main_dep_id:
-            continue
-        add_chiefs_list(dep_id, department_hash, user_id, users_hash, seen_heads, chiefs)
-
-    return chiefs
-
-
 def safe_int(x):
     """ Преобразуем любой тип данных в int (если ошибка - то возвращает None)"""
 
@@ -58,34 +25,3 @@ def safe_int(x):
         return int(x)
     except (TypeError, ValueError):
         return None
-
-
-def get_department_depth(dep_id, department_hash):
-    """Рекурсивно вычисляем глубину отдела до корня (самого главного отдела)"""
-
-    dep = department_hash.get(dep_id)
-    if not dep:
-        return 0
-    parent_id = dep.get('PARENT')
-    if not parent_id or safe_int(parent_id) not in department_hash:
-        return 1
-    return 1 + get_department_depth(safe_int(parent_id), department_hash)
-
-
-def add_chiefs_list(dep_id, department_hash, user_id, users_hash, seen_heads, chiefs):
-    """ Изменяет список chiefs """
-
-    current_dep = department_hash.get(dep_id)
-    while current_dep:
-        head_id = safe_int(current_dep.get('UF_HEAD'))
-        parent_id = safe_int(current_dep.get('PARENT'))
-
-        if head_id and head_id != user_id and head_id in users_hash and head_id not in seen_heads:
-            head_user = users_hash[head_id]
-            chiefs.append(head_user)
-            seen_heads.add(head_id)
-
-        if parent_id and parent_id in department_hash:
-            current_dep = department_hash[parent_id]
-        else:
-            current_dep = None
--- app3_workers/views.py
+++ app3_workers/views.py
@@ -1,40 +1,104 @@
-from django.http import HttpResponse
+from collections import defaultdict, deque
 
+from django.http import HttpResponse
 
 from django.shortcuts import render
 
 from integration_utils.bitrix24.bitrix_user_auth.main_auth import main_auth
 
-from .services import api_workers_info, get_department_names, chief_forward_list
+from .services import api_workers_info, safe_int
 
 
 # Create your views here.
 @main_auth(on_cookies=True)
 def workers(request):
-    but = request.bitrix_user_token
-
-    api_info = api_workers_info(but)
+    bitrix_token = request.bitrix_user_token
+    api_info = api_workers_info(bitrix_token)
 
     active_workers_list = api_info['active_workers_list']
-    # хеш для работников по id
-    users_by_id = {int(user['ID']): user for user in active_workers_list}
+    users_by_id = {safe_int(u['ID']): u for u in active_workers_list}
 
     department_list = api_info['department_list']
-    # хеш для отделов по id
-    departments_by_id = {int(dep['ID']): dep for dep in department_list}
+    departments_by_id = {safe_int(d['ID']): d for d in department_list}
 
+    # добавляем children и level для каждого отдела
+    for dep in departments_by_id.values():
+        dep['children'] = []
+        dep['level'] = 0
+
+    # заполняем children
+    for dep_id, dep in departments_by_id.items():
+        parent_id = safe_int(dep.get('PARENT'))
+        if parent_id and parent_id in departments_by_id:
+            # Добавляем как int, не строку!
+            departments_by_id[parent_id]['children'].append(dep_id)
+
+    # рекурсивная функция для уровней
+    def assign_levels(dep_id, level=0):
+        departments_by_id[dep_id]['level'] = level
+        for child_id in departments_by_id[dep_id]['children']:
+            assign_levels(child_id, level + 1)
+
+    # стартуем с корней (у кого нет PARENT)
+    roots = [dep_id for dep_id, dep in departments_by_id.items() if not dep.get('PARENT')]
+    for root in roots:
+        assign_levels(root)
+
+    # --- вспомогательные функции ---
+    def get_all_children(dep_id):
+        result = []
+        for child_id in departments_by_id[dep_id]['children']:
+            result.append(child_id)
+            result.extend(get_all_children(child_id))
+        return result
+
+    def get_department_chain_for_user(user_deps):
+        user_deps = [safe_int(d) for d in user_deps if d in departments_by_id]
+        if not user_deps:
+            return []
+        max_level = max(departments_by_id[d]['level'] for d in user_deps)
+        result = set()
+        for level in range(max_level, -1, -1):
+            for dep_id, dep in departments_by_id.items():
+                if dep['level'] == level:
+                    all_children = get_all_children(dep_id)
+                    if dep_id in user_deps or any(c in user_deps for c in all_children):
+                        result.add(dep_id)
+                        parent = dep.get('PARENT')
+                        while parent:
+                            parent = safe_int(parent)
+                            result.add(parent)
+                            parent = departments_by_id.get(parent, {}).get('PARENT')
+        return sorted(result, key=lambda d: departments_by_id[d]['level'], reverse=True)
+
+    # --- основная логика ---
     result = {}
-    for user in active_workers_list:
-        user_id = int(user['ID'])
-        department_names = get_department_names(departments_by_id, user)
-        chiefs = chief_forward_list(departments_by_id, users_by_id, user)
-        chief_names = [f"{c['NAME']} {c['LAST_NAME']}" for c in chiefs]
-        result[user_id] = {'NAME': user.get('NAME'),
-                           'LAST_NAME': user.get('LAST_NAME'),
-                           'DEPARTMENTS': department_names,
-                           'CHIEFS': chief_names}
+
+    for user_id, user in users_by_id.items():
+        user_deps = [safe_int(dep_id) for dep_id in user.get('UF_DEPARTMENT', []) if dep_id]
+        dep_chain = get_department_chain_for_user(user_deps)
+        dep_names = [departments_by_id[d]['NAME'] for d in dep_chain if d in departments_by_id]
+
+        boss_list = []
+        for dep_id in dep_chain:
+            dep = departments_by_id.get(dep_id)
+            head_id = safe_int(dep.get('UF_HEAD'))
+            if head_id and head_id in users_by_id and head_id != user_id:
+                boss_list.append(users_by_id[head_id])
+
+        boss_name_list = list(dict.fromkeys(
+            [f"{boss['NAME']} {boss['LAST_NAME']}" for boss in boss_list]
+        ))
+
+        result[user_id] = {
+            'NAME': user.get('NAME'),
+            'LAST_NAME': user.get('LAST_NAME'),
+            'DEPARTMENTS': dep_names,
+            'CHIEFS': boss_name_list
+        }
 
     return render(request, 'app3_workers/workers.html', locals())
+
 
 
 @main_auth(on_cookies=True)
