d94da5d0bb0d6fe40b8720d5eddd73f01319f17c
Александр <romanovsasa26@gmail.com>
2025-10-21 19:15:50 +0300

    подправил яндекс карту в iframe

--- app4_map/views.py
+++ app4_map/views.py
@@ -14,25 +14,37 @@ def map(request):
     but = request.bitrix_user_token
     api_inf = api_info(but)
     company_list = api_inf['company_list']
-    # хеш по id
-    company_list_by_id = {int(comp['ID']): comp for comp in company_list}
-
     address_list = api_inf['address_list']
-    # хеш по entity_id
+
+    # хеши
+    company_list_by_id = {int(comp['ID']): comp for comp in company_list}
     address_list_by_id = {int(addr['ENTITY_ID']): addr for addr in address_list}
 
     result = {}
     for company_id, company in company_list_by_id.items():
-        full_address_name = (f"{(address_list_by_id.get(company_id) or {}).get('ADDRESS_1') or ''}, "
-                             f"{(address_list_by_id.get(company_id) or {}).get('ADDRESS_2') or ''}, "
-                             f"{(address_list_by_id.get(company_id) or {}).get('CITY') or ''}, "
-                             f"{(address_list_by_id.get(company_id) or {}).get('REGION') or ''}, "
-                             f"{(address_list_by_id.get(company_id) or {}).get('PROVINCE') or ''}, "
-                             f"{(address_list_by_id.get(company_id) or {}).get('COUNTRY') or ''}")
+        addr = address_list_by_id.get(company_id, {})
+        full_address_name = ", ".join(
+            filter(None, [
+                addr.get('ADDRESS_1'),
+                addr.get('ADDRESS_2'),
+                addr.get('CITY'),
+                addr.get('REGION'),
+                addr.get('PROVINCE'),
+                addr.get('COUNTRY'),
+            ])
+        )
         result[company_id] = {'name': company["TITLE"], 'address': full_address_name}
-        result_json = urllib.parse.quote(json.dumps(result))
-        external_url = f'http://localhost:3000/test/?data={result_json}'
-    return render(request, 'app4_map/map.html', {'result': result, 'external_url': external_url})
+
+    # JSON для передачи в шаблон
+    result_json = json.dumps(result, ensure_ascii=False)
+    # external_url для перехода
+    result_encoded = urllib.parse.quote(result_json)
+    external_url = f'http://localhost:3000/test/?data={result_encoded}'
+
+    return render(request, 'app4_map/map.html', {
+        'result_json': result_json,
+        'external_url': external_url,
+    })
 
 
 def external_map(request):
